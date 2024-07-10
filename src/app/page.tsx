6dec43d25c6a17dfb30964f9c9f0185622e04836
iwmartnz <iwmartnz@gmail.com>
2024-07-09 20:57:02 -0600

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image';
+
+export default function Home() {
+    return (
+        <main className=''>
+            <h1 className='text-4xl sm:text-5xl'>Nique</h1>
+        </main>
+    );
+}
