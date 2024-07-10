e81a655c734aa2472f3621feee7ee239dc0ae8f0
iwmartnz <iwmartnz@gmail.com>
2024-07-09 21:32:02 -0600

    Created the home page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
-import Image from 'next/image';
+import Logo from '@/components/Logo';
+import Heading from '@/components/Heading';
 
 export default function Home() {
     return (
-        <main className=''>
-            <h1 className='text-4xl sm:text-5xl'>Nique</h1>
+        <main className="relative top-0 flex min-h-screen w-full flex-col items-center bg-[url('/images/restaurant/home.webp')] bg-cover bg-center">
+            <Logo />
+            <Heading variant='h1' className='animate-fade-down pt-60'>
+                The pure taste of
+            </Heading>
+            <Heading
+                variant='h2'
+                className='animate-fade-up text-6xl sm:text-8xl md:text-9xl lg:text-[160px]'
+            >
+                Thailand
+            </Heading>
+            <p className='text-brand-gray-dark max-w-md px-4 text-center'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore.
+            </p>
         </main>
     );
 }
